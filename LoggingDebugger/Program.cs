82312064aec9a31f6d2e617456b1563b55bd4101
Egbert Enfield <starlinsoft5312187@gmail.com>
2022-06-14 01:03:27 +0900

    プロジェクト ファイルを追加します。

---
+++ LoggingDebugger/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Logging;
+
+namespace LoggingDebugger {
+    internal class Program {
+        static void Main(string[] args) {
+            var logger = new Logger() {
+                Formatter = new
+            };
+            logger.Critical("WE'LL BE RIGHT BACK");
+            Console.ReadKey();
+        }
+    }
+}
